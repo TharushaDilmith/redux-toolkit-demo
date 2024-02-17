ac93c68d6c4c9ececa9e1ed9119275ba6da151f9
TharushaDilmith <tharushadilmith99@gmail.com>
2024-02-17 10:18:46 +0530

    first commit

---
+++ features/cake/cakeSlice.js
@@ -0,0 +1,18 @@
+const createSlice = require('@reduxjs/toolkit').createSlice;
+
+const initialState = {
+    numOfCakes: 10
+};
+
+const cakeSlice = createSlice({
+    name: 'cake',
+    initialState,
+    reducers: {
+        buyCake: (state, action) => {
+            state.numOfCakes -= action.payload;
+        },
+        addCake : (state, action) => {
+            state.numOfCakes += action.payload;
+        } 
+    }
+});
